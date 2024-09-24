73083c1e69947a263e4ddcde8e7aabb32c3f09e5
Elle Najt <lnajt4@gmail.com>
2024-09-23 21:33:42 -0400

    Initial commit

---
+++ publish.el
@@ -0,0 +1,19 @@
+(require 'ox-publish)
+(setq org-publish-project-alist
+      '(("my_blog"
+         :base-directory "~/org/blog"
+         :publishing-directory "~/org/blog/html"
+         :recursive t
+         :publishing-function org-html-publish-to-html
+         ;; :html-link-home "index.html"
+         ;; by default this finds the index in the folder
+         ;; which isn't the home behavior I'd expect or want
+         :html-link-use-abs-url nil
+         :html-link-org-files-as-html t
+         :with-toc nil
+         :section-numbers nil
+         :html-extension "html"
+         ;; :auto-preamble t
+         )))
+
+(org-publish-all t)
